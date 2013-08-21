e58a652f5d3dc8e0b829390e3a27e40d64400054
François-Michel De Rainville <f.derainville@gmail.com>
2013-08-21 08:34:00 -0400

    Changed download url

--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@ from distutils.core import setup, Extension
 
 ghalton_module = Extension("_ghalton", sources=["src/Halton_wrap.cxx", "src/Halton.cpp"])
 
+version = "0.6"
+
 setup (name = "ghalton",
-       version = "0.6",
+       version = version,
        author = "Francois-Michel De Rainville",
        author_email = "f.derainville@gmail.com",
        license = "LICENSE.txt",
        description = "Generalized Halton number generator",
        long_description = open("README.md").read(),
        url='https://github.com/fmder/ghalton',
-       download_url = "https://github.com/fmder/ghalton/archive",
+       download_url = "https://github.com/fmder/ghalton/tarball/master#egg=ghalton-%s" % version,
        classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
