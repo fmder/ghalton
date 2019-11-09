8fde4001d6cd670b2a3df5c888d3c098982bde59
François-Michel De Rainville <f.derainville@gmail.com>
2019-11-09 08:31:59 -0500

    Tell setuptools about c++

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 from setuptools import setup, find_packages, Extension
 
 ghalton_module = Extension("ghalton._ghalton_wrapper",
+                           language = "c++",
                            sources=["src/Halton_wrap.cxx", "src/Halton.cpp"],
                            extra_compile_args=["-stdlib=libc++"],
                            extra_link_args=['-stdlib=libc++'])
