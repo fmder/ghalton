e0943f43478b4e8076ca5f1e250344ea0cca0310
François-Michel De Rainville <f.derainville@gmail.com>
2019-11-09 08:56:01 -0500

    Try removing c++ flags

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from setuptools import setup, find_packages, Extension
 ghalton_module = Extension("ghalton._ghalton_wrapper",
                            language = "c++",
                            sources=["src/Halton_wrap.cxx", "src/Halton.cpp"],
-                           extra_compile_args=["-stdlib=libstdc++"],
-                           extra_link_args=['-stdlib=libstdc++'])
+#                            extra_compile_args=["-stdlib=libstdc++"],
+#                            extra_link_args=['-stdlib=libstdc++']
+                          )
 
 version = "0.6.2"
 
